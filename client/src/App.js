8992fad5b10fb1faca26b86bd26546ce766899b6
Shakil <Shakil2015>
2020-02-20 00:49:32 +0600

    git setting

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+
+
+
+const App = ()=>{
+
+   return (
+    <Router>  
+      <Route exact path ="/"  component={Join}/>
+      <Route path ="/Chat" component={Chat}/>
+   </Router>
+   )
+        
+    
+    
+
+}
+
+export default App;
