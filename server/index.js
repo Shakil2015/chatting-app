dfaabd46a1df4af0c15012f37538599adc261192
Shakil2015 <sshakil496@gmail.com>
2020-02-20 18:14:29 +0600

    office20

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const http = require('http');
 
 const port = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app =express();
 const server = http.createServer(app);
 const io =socketio(server);
+
+app.use(router);
 
 server.listen(port, ()=> console.log(`server connected ${port}`));
